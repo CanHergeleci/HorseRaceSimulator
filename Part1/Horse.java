c8dbb2a6a209676d2fc705955f5d4e40ca3f34ec
Can Hergeleci <canhergeleci@outlook.com>
2025-04-23 12:49:22 +0100

    Fixed setConfidence method to only accept confidence value between 0 and 1.

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -68,7 +68,14 @@ public class Horse
 
     public void setConfidence(double newConfidence)
     {
-        this.confidence = newConfidence;
+        if (newConfidence < 0 || newConfidence > 1)
+        {
+            throw new IllegalArgumentException("Confidence must be between 0 and 1");
+        }
+        else
+        {
+            this.confidence = newConfidence;
+        }
     }
     
     public void setSymbol(char newSymbol)
