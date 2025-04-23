33a121fc8451a53a10f6907a79b20272511c9ab3
Can Hergeleci <canhergeleci@outlook.com>
2025-04-23 13:53:05 +0100

    Created class HorseTest which tests all fields and set/ get methods in Horse class

---
+++ Part1/HorseTest.java
@@ -0,0 +1,48 @@
+public class HorseTest {
+    public static void main(String[] args)
+    {
+        // Create new Horse object
+        Horse horse1 = new Horse('♘', "QueenMary Horse", 0.8);
+
+        // Test getName method
+        System.out.println("Expected Horse Name: QueenMary Horse");
+        System.out.println("Actual Horse Name: " + horse1.getName());
+
+        // Test getSymbol method
+        System.out.println("Expected Horse Symbol: ♘");
+        System.out.println("Actual Horse Symbol: " + horse1.getSymbol());
+
+        // Test setSymbol method
+        System.out.println("Expected Horse Symbol after setting new symbol: &");
+        horse1.setSymbol('&');
+        System.out.println("Actual Horse Symbol after setting new symbol: " + horse1.getSymbol());
+
+        // Test getConfidence method
+        System.out.println("Expected Horse Confidence: 0.8");
+        System.out.println("Actual Horse Confidence: " + horse1.getConfidence());
+
+        // Test getDistanceTravelled method
+        System.out.println("Expected Distance Travelled: 0");
+        System.out.println("Actual Distance Travelled: " + horse1.getDistanceTravelled());
+
+        // Test moveForward method
+        System.out.println("Expected Distance Travelled after moving forward: 2");
+        horse1.moveForward();
+        horse1.moveForward();
+        System.out.println("Actual Distance Travelled after moving forward: " + horse1.getDistanceTravelled());
+
+        // Test goBackToStart method
+        System.out.println("Expected Distance Travelled after going back to start: 0");
+        horse1.goBackToStart();
+        System.out.println("Actual Distance Travelled after going back to start: " + horse1.getDistanceTravelled());
+
+        // Test hasFallen method
+        System.out.println("Expected Horse Fallen: false");
+        System.out.println("Actual Horse Fallen: " + horse1.hasFallen());
+
+        // Test fall method
+        System.out.println("Expected Horse Fallen after falling: true");
+        horse1.fall();
+        System.out.println("Actual Horse Fallen after falling: " + horse1.hasFallen());
+    }
+}
