18adb6bcddc1e58b049430164139ba8c46b912ab
Can Hergeleci <canhergeleci@outlook.com>
2025-04-24 14:40:31 +0100

    Updated testing class to match new race class parameter requirements.

--- Part1/RaceTest.java
+++ Part1/RaceTest.java
@@ -2,7 +2,7 @@ public class RaceTest
 {
     public static void main(String[] args)
     {
-        Race race = new Race(20);
+        Race race = new Race(20, 5);
         race.addHorse(new Horse('♘', "QueenMary Horse", 0.8), 1);
         race.addHorse(new Horse('♞', "KingHorse", 0.7), 2);
         race.addHorse(new Horse('0', "FastHorse", 0.5), 3);
