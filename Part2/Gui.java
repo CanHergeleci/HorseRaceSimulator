9178edb569a7af0dfd98a8e5fcaf1b6a52904ac3
Can Hergeleci <canhergeleci@outlook.com>
2025-04-25 20:48:25 +0100

    Created main frame with tabs that lead to different panels to represent each section of the Horse Race Simulator

--- Part2/Gui.java
+++ Part2/Gui.java
@@ -1,3 +1,28 @@
+import javax.swing.*;
+
 public class Gui {
-    
+    public static void main (String[] args)
+    {
+        // Create a JFrame
+        JFrame frame = new JFrame("Horse Race Track");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 600);
+        
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        // Race config tab
+        tabbedPane.addTab("Race Configuration", RaceConfigPanel());
+
+        // Race tab
+        tabbedPane.addTab("Ongoing Race", RacePanel());
+
+        // Statistics & Analytics tab
+        tabbedPane.addTab("Statistics", StatisticsPanel());
+
+        // Betting tab
+        tabbedPane.addTab("Betting", BettingPanel());
+
+        frame.add(tabbedPane);
+        frame.setVisible(true);
+    }
 }
